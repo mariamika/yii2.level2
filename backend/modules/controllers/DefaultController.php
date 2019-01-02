3761ad66570515e2886a69aed70f8f1127efb2aa
mariamika <joolveg@gmail.com>
2019-01-03 02:50:15 +0300

    Отчет по задачам для админа.

--- backend/modules/controllers/DefaultController.php
+++ backend/modules/controllers/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace backend\modules\controllers;
 
+use common\models\Tasks;
+use frontend\modules\tasks\controllers\ListController;
+use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 
 /**
@@ -15,6 +18,41 @@ class DefaultController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $model_tasks = ListController::findModel();
+        $dataProvider1 = new ActiveDataProvider([
+            'query' => $model_tasks,
+            'pagination' => [
+                'pageSize' => 5
+            ]
+        ]);
+
+        $model_overdue_tasks = Tasks::find()
+            ->joinWith(['performer','user'])
+            ->select(['tasks.*','performer.*','user.*'])
+            ->andWhere('tasks.dateDeadline > DATE_SUB(CURDATE(), INTERVAL 7 DAY)')
+            ->andWhere('tasks.statusTask != 0');
+        $dataProvider3 = new ActiveDataProvider([
+            'query' => $model_overdue_tasks,
+            'pagination' => [
+                'pageSize' => 5
+            ]
+        ]);
+
+        $model_end_tasks = Tasks::find()
+            ->joinWith(['performer','user'])
+            ->select(['tasks.*','performer.*','user.*'])
+            ->andWhere('tasks.statusTask = 0');;
+        $dataProvider2 = new ActiveDataProvider([
+            'query' => $model_end_tasks,
+            'pagination' => [
+                'pageSize' => 5
+            ]
+        ]);
+
+        return $this->render('index',[
+            'model_tasks' => $dataProvider1,
+            'model_end_tasks' => $dataProvider2,
+            'model_overdue_tasks' => $dataProvider3,
+        ]);
     }
 }
--- backend/modules/views/default/index.php
+++ backend/modules/views/default/index.php
@@ -1,11 +1,187 @@
 <?php
-use yii\bootstrap\Html;?>
+use yii\bootstrap\Html;
+use yii\grid\GridView;
+/* @var $model_tasks yii\data\ActiveDataProvider
+ * @var $model_end_tasks yii\data\ActiveDataProvider
+ * @var $model_overdue_tasks yii\data\ActiveDataProvider
+ */
+?>
 
 <div class="admin-default-index">
-    <?php echo Html::a('Projects',['/admin/project'])?>
-    <br>
-    <?php echo Html::a('Tasks',['/admin/task']);?>
-    <br>
-    <?php echo Html::a('Performers',['/admin/performer'])?>
+    <?php echo Html::a('Projects',['/admin/project'],['class' => 'btn btn-primary'])?>
+
+    <?php echo Html::a('Tasks',['/admin/task'],['class' => 'btn btn-primary']);?>
+
+    <?php echo Html::a('Performers',['/admin/performer'],['class' => 'btn btn-primary'])?>
 </div>
 
+<div style="padding: 1px 5px 1px 15px;
+            margin-top: 50px;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;"
+     class="p-3 mb-2 bg-primary text-white">
+    <h3>Полный список задач</h3>
+</div>
+<div style="border-left: #0d6aad 1px solid;
+            border-right: #0d6aad 1px solid;
+            border-bottom: #0d6aad 1px solid;">
+    <?= GridView::widget(['dataProvider' => $model_tasks,
+        'columns' => [['attribute' => 'taskName',
+            'label' => Yii::t('app','Task Name'),
+            'value' => 'taskName'],
+            ['attribute' => 'statusTask',
+                'filter' => [0 => 'Close',
+                    1 => 'Active'
+                ],
+                'label' => Yii::t('app','Status'),
+                'format' => 'raw',
+                'value' => function ($model, $key, $index, $column) {
+        $active = $model->{$column->attribute} === 1;
+        return $active ? '<span class="label label-success">Active</span>' : '<span class="label label-danger">Close</span>';
+        },
+            ],
+            ['attribute' => 'priority',
+                'filter' => [1 => 'Высокий приоритет',
+                    2 => 'Средний приоритет',
+                    3 => 'Низкий приоритет',
+                    4 => 'Необязательно к испольнению'
+                ],
+                'label' => Yii::t('app','Priority'),
+                'format' => 'raw',
+                'value' => function ($model, $key, $index, $column){
+        $arr_search = [
+                1 => 'Высокий приоритет',
+            2 => 'Средний приоритет',
+            3 => 'Низкий приоритет',
+            4 => 'Необязательно'
+        ];
+
+        if (array_key_exists($model->{$column->attribute},$arr_search)){
+            return '<span class="label label-primary">' . $arr_search[$model->{$column->attribute}] . '</span>';
+        }
+    }
+    ],
+            ['attribute' => 'performer',
+                'label' => Yii::t('app','Performer'),
+                'value' => 'performer.name'],
+            ['attribute' => 'project',
+                'label' => Yii::t('app','Project'),
+                'value' => 'project.projectName']
+        ],
+    ]); ?>
+</div>
+
+<div style="padding: 1px 5px 1px 15px;
+            margin-top: 50px;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            background-color: #994d53;
+            color: #FFFFFF">
+    <h3>Просроченные задачи</h3>
+</div>
+<div style="border-left: #994d53 1px solid;
+            border-right: #994d53 1px solid;
+            border-bottom: #994d53 1px solid;">
+    <?= GridView::widget(['dataProvider' => $model_overdue_tasks,
+        'columns' => [['attribute' => 'taskName',
+            'label' => Yii::t('app','Task Name'),
+            'value' => 'taskName'],
+            ['attribute' => 'statusTask',
+                'filter' => [0 => 'Close',
+                    1 => 'Active'
+                ],
+                'label' => Yii::t('app','Status'),
+                'format' => 'raw',
+                'value' => function ($model, $key, $index, $column) {
+                    $active = $model->{$column->attribute} === 1;
+                    return $active ? '<span class="label label-success">Active</span>' : '<span class="label label-danger">Close</span>';
+                },
+            ],
+            ['attribute' => 'priority',
+                'filter' => [1 => 'Высокий приоритет',
+                    2 => 'Средний приоритет',
+                    3 => 'Низкий приоритет',
+                    4 => 'Необязательно к испольнению'
+                ],
+                'label' => Yii::t('app','Priority'),
+                'format' => 'raw',
+                'value' => function ($model, $key, $index, $column){
+                    $arr_search = [
+                        1 => 'Высокий приоритет',
+                        2 => 'Средний приоритет',
+                        3 => 'Низкий приоритет',
+                        4 => 'Необязательно'
+                    ];
+
+                    if (array_key_exists($model->{$column->attribute},$arr_search)){
+                        return '<span class="label label-primary">' . $arr_search[$model->{$column->attribute}] . '</span>';
+                    }
+                }
+            ],
+            ['attribute' => 'performer',
+                'label' => Yii::t('app','Performer'),
+                'value' => 'performer.name'],
+            ['attribute' => 'project',
+                'label' => Yii::t('app','Project'),
+                'value' => 'project.projectName']
+        ],
+    ]); ?>
+</div>
+
+
+
+<div style="padding: 1px 5px 1px 15px;
+            margin-top: 50px;
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+            background-color: #f39c12">
+    <h3>Завершенные задачи</h3>
+</div>
+<div style="border-left: #f39c12 1px solid;
+            border-right: #f39c12 1px solid;
+            border-bottom: #f39c12 1px solid;">
+    <?= GridView::widget(['dataProvider' => $model_end_tasks,
+        'columns' => [['attribute' => 'taskName',
+            'label' => Yii::t('app','Task Name'),
+            'value' => 'taskName'],
+            ['attribute' => 'statusTask',
+                'filter' => [0 => 'Close',
+                    1 => 'Active'
+                ],
+                'label' => Yii::t('app','Status'),
+                'format' => 'raw',
+                'value' => function ($model, $key, $index, $column) {
+                    $active = $model->{$column->attribute} === 1;
+                    return $active ? '<span class="label label-success">Active</span>' : '<span class="label label-danger">Close</span>';
+                },
+            ],
+            ['attribute' => 'priority',
+                'filter' => [1 => 'Высокий приоритет',
+                    2 => 'Средний приоритет',
+                    3 => 'Низкий приоритет',
+                    4 => 'Необязательно к испольнению'
+                ],
+                'label' => Yii::t('app','Priority'),
+                'format' => 'raw',
+                'value' => function ($model, $key, $index, $column){
+                    $arr_search = [
+                        1 => 'Высокий приоритет',
+                        2 => 'Средний приоритет',
+                        3 => 'Низкий приоритет',
+                        4 => 'Необязательно'
+                    ];
+
+                    if (array_key_exists($model->{$column->attribute},$arr_search)){
+                        return '<span class="label label-primary">' . $arr_search[$model->{$column->attribute}] . '</span>';
+                    }
+                }
+            ],
+            ['attribute' => 'performer',
+                'label' => Yii::t('app','Performer'),
+                'value' => 'performer.name'],
+            ['attribute' => 'project',
+                'label' => Yii::t('app','Project'),
+                'value' => 'project.projectName']
+        ],
+    ]); ?>
+</div>
